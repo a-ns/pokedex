89fb01ba91ddafc8e12ebd783b4464d1e47139ed
a-ns <exmann94@gmail.com>
2017-10-12 21:45:38 -0500

    added search bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { withRouter } from "react-router-dom";
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchValue: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log("here", e.target);
+    this.props.history.push(`/${this.state.searchValue}`);
+    this.setState({ searchValue: "" });
+  }
+
+  handleChange(e) {
+    this.setState({ searchValue: e.target.value });
+  }
   render() {
-    return <main>{this.props.children}</main>;
+    return [
+      <nav key="1">
+        <form onSubmit={this.handleSubmit}>
+          <input
+            placeholder="Enter pokemon"
+            type="text"
+            value={this.state.searchValue}
+            onChange={this.handleChange}
+          />
+        </form>
+      </nav>,
+      <main key="2">{this.props.children}</main>
+    ];
   }
 }
 
-export default App;
+export default withRouter(App);
